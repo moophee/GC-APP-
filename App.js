ffdf0c5b05da1eb462af1ef1f2f240ccc515eaf0
moophee <moophee.w@gmail.com>
2025-01-18 10:54:06 +0700

    App.js

--- App.js
+++ App.js
@@ -8,22 +8,28 @@ export default function App(){
     const [Dev,setDev] = useState({name:"Meepooh",year:2023})
     const [data,setdata]=useState([
       { id:1,name : "A",age:16,sex:"ale"},
-      { id:1,name : "B",age:15,sex:"Male"},
-      { id:1,name : "C",age:15,sex:"Male"},
-      { id:1,name : "D",age:15,sex:"Male"},
-      { id:1,name : "E",age:16,sex:"Female"},
-      { id:1,name : "F",age:16,sex:"Female"},
-      { id:1,name : "G",age:16,sex:"Male"},
-      { id:1,name : "H,",age:16,sex:"Male"},
+      { id:2,name : "B",age:15,sex:"Male"},
+      { id:3,name : "C",age:15,sex:"Male"},
+      { id:4,name : "D",age:15,sex:"Male"},
+      { id:5,name : "E",age:16,sex:"Female"},
+      { id:6,name : "F",age:16,sex:"Female"},
+      { id:7,name : "G",age:16,sex:"Male"},
+      { id:8,name : "H,",age:16,sex:"Male"},
 
     ])
+
+    const deldata=(id)=>{
+      setdata((prevdata)=>{
+        return prevdata.filter((item)=>item.id!=id);
+      })  
+    }
     return(
       <ScrollView>
         <View style={myStyle.container}>
         <FlatList 
           data={data}
           renderItem={({item})=>(
-            <Person item={item}/>
+            <Person item={item} deldata={deldata}/>
           )}
         keyExtractor={(item) => item.id.toString()}
         ListHeaderComponent={<Text style={myStyle.header}>ข้อมูล</Text>}
