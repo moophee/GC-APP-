31b9e07b9cbc6e4ed70e2d96a906f462dc21d38a
moophee <moophee.w@gmail.com>
2025-01-18 11:25:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,41 +1,40 @@
 import { FlatList } from 'react-native';
 import { myStyle } from './styles/myStyle';
 import { useState } from 'react';
-import { Text, View, Image, ScrollView, Button} from 'react-native';
+import { Text, View, ScrollView, Button } from 'react-native';
 import Person from './components/Person';
-
-export default function App(){
-    const [Dev,setDev] = useState({name:"Meepooh",year:2023})
-    const [data,setdata]=useState([
-      { id:1,name : "A",age:16,sex:"ale"},
-      { id:2,name : "B",age:15,sex:"Male"},
-      { id:3,name : "C",age:15,sex:"Male"},
-      { id:4,name : "D",age:15,sex:"Male"},
-      { id:5,name : "E",age:16,sex:"Female"},
-      { id:6,name : "F",age:16,sex:"Female"},
-      { id:7,name : "G",age:16,sex:"Male"},
-      { id:8,name : "H,",age:16,sex:"Male"},
-
-    ])
-
-    const deldata=(id)=>{
-      setdata((prevdata)=>{
-        return prevdata.filter((item)=>item.id!=id);
-      })  
-    }
-    return(
-      <ScrollView>
-        <View style={myStyle.container}>
-        <FlatList 
-          data={data}
-          renderItem={({item})=>(
-            <Person item={item} deldata={deldata}/>
+import AddForm from './components/addform';
+export default function App() {
+    const [Dev, setDev] = useState({ name: "Meepooh", year: 2023 });
+    const [data, setData] = useState([
+        { id: 1, name: "Copter", age: 16, sex: "Male" },
+        { id: 2, name: "Peking", age: 15, sex: "Male" },
+        { id: 3, name: "Dawn", age: 15, sex: "Male" },
+        { id: 4, name: "Boston", age: 15, sex: "Male" },
+        { id: 5, name: "Noon", age: 16, sex: "Female" },
+        { id: 6, name: "Proud", age: 16, sex: "Female" },
+        { id: 7, name: "Captain", age: 16, sex: "Male" },
+        { id: 8, name: "Prom", age: 16, sex: "Male" },
+    ]);
+    const deldata = (id) => {
+        console.log("Attempting to delete item with id:", id);
+        setData((prevData) => {
+            const updatedData = prevData.filter((item) => item.id !== id);
+            console.log("Updated data:", updatedData);
+            return updatedData;
+        });
+    };
+    return (
+      <View style={myStyle.container}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+            <Person item={item} deldata={deldata} />
           )}
         keyExtractor={(item) => item.id.toString()}
         ListHeaderComponent={<Text style={myStyle.header}>ข้อมูล</Text>}
-        ListEmptyComponent={<Text>ไม่มีข้อมูล</Text>}
-        />
-        </View>
-      </ScrollView>
+        ListEmptyComponent={<Text>ไม่มีข้อมูล</Text>}/>
+      <AddForm/>
+      </View>
     );
-} 
+}
